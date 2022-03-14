6201b9afd0d4a0dc55b6f2bb960ef005cbc5c94d
lemon2123 <t.saito602@gmail.com>
2022-03-14 18:47:26 +0900

    ユーザー管理機能　修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
     first_name_zen        {'太郎'}
     last_name_kana        {'タナカ'}
     first_name_kana       {'タロウ'}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation {password}
     birthday              {'2000-01-01'}
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
+    context '新規登録できるとき' do
+      it '全て存在すれば登録できる' do
+        expect(@user).to be_valid
+      end
+    end
+    context '新規登録できないとき' do
     it 'nicknameが空では登録できない' do
       @user.nickname = ''
       @user.valid?
@@ -101,5 +107,6 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include("Birthday can't be blank")
     end
+  end
   end
 end
