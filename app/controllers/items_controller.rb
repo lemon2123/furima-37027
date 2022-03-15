4c1eae4b51bc99ed20aee0bc84452d57bf3050ee
lemon2123 <t.saito602@gmail.com>
2022-03-15 22:32:07 +0900

    ActiveStorage導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,15 @@
 class ItemsController < ApplicationController
+
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :explanation, :detail_category_id, :detail_status_id, :delivery_id, :delivery_area_id, :delivery_day_id, :price).merge(user_id: current_user.id)
+  end
+
 end
