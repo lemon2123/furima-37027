3e6ead895469bf8ec6802d46eec3148a0625e656
lemon2123 <t.saito602@gmail.com>
2022-03-16 17:04:19 +0900

    ActiveHashの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,7 +7,17 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
+
   def item_params
     params.require(:item).permit(:image, :name, :explanation, :detail_category_id, :detail_status_id, :delivery_id, :delivery_area_id, :delivery_day_id, :price).merge(user_id: current_user.id)
   end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,25 @@
 class Item < ApplicationRecord
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :status
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :delivery
+  validates :delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :area
+  validates :area_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :Day
+  validates :day_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+
 end
