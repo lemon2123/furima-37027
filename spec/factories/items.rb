d93eeeb060b918d652f785e9b4d24ce77781651c
lemon2123 <t.saito602@gmail.com>
2022-03-19 16:17:29 +0900

    商品出品の単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    association :user
+    name                 {'色鉛筆'}
+    explanation          {'色鉛筆3本セットです'}
+    detail_category_id   {2}
+    detail_status_id     {2}
+    delivery_id          {2}
+    delivery_area_id     {2}
+    delivery_day_id      {2}
+    price                {500}
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
