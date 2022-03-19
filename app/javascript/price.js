482d111df4e4b2471597c21084cd5bca2a998d1f
lemon2123 <t.saito602@gmail.com>
2022-03-19 18:13:04 +0900

    商品出品機能　修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -3,7 +3,6 @@ window.addEventListener('load', function(){
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", function(){
     const inputValue = priceInput.value;
-    console.log(inputValue);
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
     const profitInput = document.getElementById("profit");
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,19 +5,15 @@ class Item < ApplicationRecord
   belongs_to :detail_category
   validates :detail_category_id, numericality: { other_than: 1 , message: "can't be blank"}
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :detail_status
   validates :detail_status_id, numericality: { other_than: 1 , message: "can't be blank"}
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :delivery
   validates :delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :delivery_area
   validates :delivery_area_id, numericality: { other_than: 1 , message: "can't be blank"}
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :delivery_day
   validates :delivery_day_id, numericality: { other_than: 1 , message: "can't be blank"}
 
