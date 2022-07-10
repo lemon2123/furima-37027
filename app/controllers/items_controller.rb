c6e452307a9d287f5f7ea74e9b8b73eba651f387
lemon2123 <t.saito602@gmail.com>
2022-07-10 20:43:25 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -43,8 +43,8 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :explanation, :detail_category_id, :detail_status_id, :delivery_id,
-                                 :delivery_area_id, :delivery_day_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit( :image, :name, :explanation, :detail_category_id, :detail_status_id, :delivery_id,
+                                  :delivery_area_id, :delivery_day_id, :price).merge(user_id: current_user.id)
   end
 
   def set_item
